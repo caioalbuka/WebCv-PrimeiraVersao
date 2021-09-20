dada1a556edb028d5f5955e02eb96502fda85934
Caio Albuka <caioalbuka@gmail.com>
2021-09-20 15:58:55 -0300

    Terceiro Commite - Finalizai a pagina, agora so falta arruamr os layouts

---
+++ src/components/Certificados/Certificados.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './style.css'
+
+function Certificados(){
+    
+
+    return(
+        
+    <><div className='certificados'>
+        Cursos e Certificados
+    </div>
+    </>
+    )
+}
+
+export default Certificados
